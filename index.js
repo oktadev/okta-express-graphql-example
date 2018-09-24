530389a8dec5a245e563136323578a94e92f5372
Braden M. Kelley <redbmk@gmail.com>
2018-09-23 19:08:41 -0700

    add posts and authors with fake data

--- index.js
+++ index.js
@@ -9,13 +9,67 @@ app.use(cors())
 
 const schema = buildASTSchema(gql`
   type Query {
-    hello: String
+    posts: [Post]
+    post(id: ID): Post
+    authors: [Person]
+    author(id: ID): Person
+  }
+
+  type Post {
+    id: ID
+    author: Person
+    body: String
+  }
+
+  type Person {
+    id: ID
+    posts: [Post]
+    firstName: String
+    lastName: String
   }
 `)
 
-const rootValue = {
-  hello: () => 'Hello, world'
+const PEOPLE = new Map()
+const POSTS = new Map()
+
+class Post {
+  constructor (data) { Object.assign(this, data) }
+  get author () {
+    return PEOPLE.get(this.authorId)
+  }
 }
+
+class Person {
+  constructor (data) { Object.assign(this, data) }
+  get posts () {
+    return [...POSTS.values()].filter(post => post.authorId === this.id)
+  }
+}
+
+const rootValue = {
+  posts: () => POSTS.values(),
+  post: ({ id }) => POSTS.get(id),
+  authors: () => PEOPLE.values(),
+  author: ({ id }) => PEOPLE.get(id)
+}
+
+const initializeData = () => {
+  const fakePeople = [
+    { id: '1', firstName: 'John', lastName: 'Doe' },
+    { id: '2', firstName: 'Jane', lastName: 'Doe' }
+  ]
+
+  fakePeople.forEach(person => PEOPLE.set(person.id, new Person(person)))
+
+  const fakePosts = [
+    { id: '1', authorId: '1', body: 'Hello world' },
+    { id: '2', authorId: '2', body: 'Hi, planet!' }
+  ]
+
+  fakePosts.forEach(post => POSTS.set(post.id, new Post(post)))
+}
+
+initializeData()
 
 app.use('/graphql', graphqlHTTP({ schema, rootValue }))
 
